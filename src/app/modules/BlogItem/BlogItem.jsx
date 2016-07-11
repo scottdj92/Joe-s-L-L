434455528ed14461550a200c01d395bdf13de9e1
sjone4 <sjones4@sapient.com>
2016-07-11 16:54:38 -0400

    Initial Scss markup

--- src/app/modules/BlogItem/BlogItem.jsx
+++ src/app/modules/BlogItem/BlogItem.jsx
@@ -8,16 +8,16 @@ class BlogItem extends React.Component{
 
     render() {
         return (
-            <div className='blog-item'>
+            <div className='blog-item columns'>
                 {/*console.log('child', this.props)*/}
-                <img src='http://placehold.it/200' alt='item 1'></img>
-                <h2>{this.props.data.title}</h2>
-                <div>
-                    <span className='blog-item__location'>{this.props.data.location}</span>
-                    |
-                    <span>{this.props.data.team}</span>
+                <div className='column'>
+                    <img src='http://placehold.it/50' alt={'item' + this.props.id}></img>
                 </div>
-                <p className='blog-item__desc'>{this.props.data.description}</p>
+                <div className='column'>
+                    <h2 className='blog-item__title'>{this.props.data.title}</h2>
+                    <span className='blog-item__location-team'>{this.props.data.location} | {this.props.data.team}</span>
+                    <p className='blog-item__desc'>{this.props.data.description}</p>
+                </div>
             </div>
         )
     }
